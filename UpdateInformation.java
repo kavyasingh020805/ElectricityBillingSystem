ba913922ce234ec31c94fea43a38d45d1e098b96
Kavya Singh <150460816+kavyasingh020805@users.noreply.github.com>
2025-03-27 11:02:17 +0530

    Update UpdateInformation.java

--- UpdateInformation.java
+++ UpdateInformation.java
@@ -3,75 +3,82 @@ package electricity.billing.system;
 import javax.swing.*;
 import java.awt.*;
 import java.sql.*;
+import java.awt.event.*;
 
-public class UpdateInformation extends JFrame {
+public class UpdateInformation extends JFrame implements ActionListener{
     
-    UpdateInformation(){
+    JTextField tfaddress, tfcity, tfstate, tfemail, tfphone;
+    JButton update, cancel;
+    String meter;
+    
+    UpdateInformation(String meter){
+        
+        this.meter = meter;
         
         setBounds(300, 150, 1050, 450);
-        setVisible(true); 
+        setLayout(null);
         getContentPane().setBackground(Color.white);
         
         JLabel heading = new JLabel("UPDATE CUSTOMER INFORMATION");
         heading.setBounds(110, 10, 500, 40);
         heading.setFont(new Font("Tahoma", Font.PLAIN, 20));
         add(heading);
-        
+
         JLabel lblname = new JLabel("Name");
         lblname.setBounds(30, 70, 100, 20);
         add(lblname);
-        
-        JLabel name = new JLabel("");
-        name.setBounds(160, 70, 100, 20);
+
+        JLabel name = new JLabel("");  
+        name.setBounds(160, 70, 200, 20);
         add(name);
-        
+
         JLabel lblmeternumber = new JLabel("Meter Number");
-        lblmeternumber.setBounds(30, 130, 100, 20);
+        lblmeternumber.setBounds(30, 110, 100, 20);
         add(lblmeternumber);
-        
-        JLabel meternumber = new JLabel("");
-        meternumber.setBounds(160, 130, 100, 20);
+
+        JLabel meternumber = new JLabel("");  
+        meternumber.setBounds(160, 110, 200, 20);
         add(meternumber);
-        
+
         JLabel lbladdress = new JLabel("Address");
-        lbladdress.setBounds(70, 200, 100, 20);
+        lbladdress.setBounds(30, 150, 100, 20);
         add(lbladdress);
-        
-        JLabel address = new JLabel("");
-        address.setBounds(200, 200, 100, 20);
-        add(address);
-        
+
+        tfaddress = new JTextField();
+        tfaddress.setBounds(160, 150, 200, 20);
+        add(tfaddress);
+
         JLabel lblcity = new JLabel("City");
-        lblcity.setBounds(70, 260, 100, 20);
+        lblcity.setBounds(30, 190, 100, 20);
         add(lblcity);
-        
-        JLabel city = new JLabel("");
-        city.setBounds(200, 260, 100, 20);
-        add(city);
-        
+
+        tfcity = new JTextField();
+        tfcity.setBounds(160, 190, 200, 20);
+        add(tfcity);
+
         JLabel lblstate = new JLabel("State");
-        lblstate.setBounds(450, 80, 100, 20);
+        lblstate.setBounds(30, 230, 100, 20);
         add(lblstate);
-        
-        JLabel state = new JLabel("");
-        state.setBounds(580, 80, 100, 20);
-        add(state);
-        
+
+        tfstate = new JTextField();
+        tfstate.setBounds(160, 230, 200, 20);
+        add(tfstate);
+
         JLabel lblemail = new JLabel("Email");
-        lblemail.setBounds(450, 140, 100, 20);
+        lblemail.setBounds(30, 270, 100, 20);
         add(lblemail);
-        
-        JLabel email = new JLabel("");
-        email.setBounds(580, 140, 100, 20);
-        add(email);
-        
+
+        tfemail = new JTextField();
+        tfemail.setBounds(160, 270, 200, 20);
+        add(tfemail);
+
         JLabel lblphone = new JLabel("Phone");
-        lblphone.setBounds(450, 200, 100, 20);
+        lblphone.setBounds(30, 310, 100, 20);
         add(lblphone);
-        
-        JLabel phone = new JLabel("");
-        phone.setBounds(580, 200, 100, 20);
-        add(phone);
+
+        tfphone = new JTextField();
+        tfphone.setBounds(160, 310, 200, 20);
+        add(tfphone);
         
         try{
             
@@ -80,11 +87,11 @@ public class UpdateInformation extends JFrame {
             
             while (rs.next()){
                 name.setText(rs.getString("name"));
-                address.setText(rs.getString("address"));
-                city.setText(rs.getString("city"));
-                state.setText(rs.getString("state"));
-                email.setText(rs.getString("email"));
-                phone.setText(rs.getString("phone"));
+                tfaddress.setText(rs.getString("address"));
+                tfcity.setText(rs.getString("city"));
+                tfstate.setText(rs.getString("state"));
+                tfemail.setText(rs.getString("email"));
+                tfphone.setText(rs.getString("phone"));
                 meternumber.setText(rs.getString("meter_no"));
             }
             
@@ -92,26 +99,37 @@ public class UpdateInformation extends JFrame {
             e.printStackTrace();
         }
         
-        //cancel = new JButton("Cancel");
-        //cancel.setBackground(Color.black);
-        //cancel.setForeground(Color.white);
-        //cancel.setBounds(350, 320, 100, 40);
-        //add(cancel);
-        //cancel.addActionListener(this);
+        update = new JButton("Update");
+        update.setBackground(Color.black);
+        update.setForeground(Color.white);
+        update.setBounds(80, 350, 100, 30);
+        add(update);
+        update.addActionListener(this);
         
-        ImageIcon i1 = new ImageIcon(ClassLoader.getSystemResource("icon/viewcustomer.jpg"));
-        Image i2 = i1.getImage().getScaledInstance(600, 300, Image.SCALE_DEFAULT);
+        cancel = new JButton("Cancel");
+        cancel.setBackground(Color.black);
+        cancel.setForeground(Color.white);
+        cancel.setBounds(200, 350, 100, 30);
+        add(cancel);
+        cancel.addActionListener(this);
+        
+        ImageIcon i1 = new ImageIcon(ClassLoader.getSystemResource("icon/update.jpg"));
+        Image i2 = i1.getImage().getScaledInstance(400, 300, Image.SCALE_DEFAULT);
         ImageIcon i3 = new ImageIcon(i2);
         JLabel image = new JLabel(i3);
-        image.setBounds(100, 350, 600, 300);
+        image.setBounds(550, 50, 400, 300);
         add(image);
         
-        setLayout(null);
+        setVisible(true); 
+        
+    }
+    
+    public void actionPerformed(ActionEvent ae){
         
     }
     
     public static void main(String[] args){
-        new UpdateInformation();
+        new UpdateInformation("");
     }
     
 }
