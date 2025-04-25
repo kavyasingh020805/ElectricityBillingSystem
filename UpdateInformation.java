f788ef3e958d72420c933eb300851f1723e0deb1
Kavya Singh <150460816+kavyasingh020805@users.noreply.github.com>
2025-04-25 21:42:52 +0530

    Update UpdateInformation.java

--- UpdateInformation.java
+++ UpdateInformation.java
@@ -5,21 +5,21 @@ import java.awt.*;
 import java.sql.*;
 import java.awt.event.*;
 
-public class UpdateInformation extends JFrame implements ActionListener{
-    
+public class UpdateInformation extends JFrame implements ActionListener {
+
     JTextField tfaddress, tfcity, tfstate, tfemail, tfphone;
     JButton update, cancel;
     String meter;
     JLabel name;
-    
-    UpdateInformation(String meter){
-        
+
+    UpdateInformation(String meter) {
+
         this.meter = meter;
-        
+
         setBounds(300, 150, 1050, 450);
         setLayout(null);
         getContentPane().setBackground(Color.white);
-        
+
         JLabel heading = new JLabel("UPDATE CUSTOMER INFORMATION");
         heading.setBounds(110, 10, 500, 40);
         heading.setFont(new Font("Tahoma", Font.PLAIN, 20));
@@ -29,7 +29,7 @@ public class UpdateInformation extends JFrame implements ActionListener{
         lblname.setBounds(30, 70, 100, 20);
         add(lblname);
 
-        name = new JLabel("");  
+        name = new JLabel("");
         name.setBounds(160, 70, 200, 20);
         add(name);
 
@@ -37,7 +37,7 @@ public class UpdateInformation extends JFrame implements ActionListener{
         lblmeternumber.setBounds(30, 110, 100, 20);
         add(lblmeternumber);
 
-        JLabel meternumber = new JLabel("");  
+        JLabel meternumber = new JLabel("");
         meternumber.setBounds(160, 110, 200, 20);
         add(meternumber);
 
@@ -80,13 +80,22 @@ public class UpdateInformation extends JFrame implements ActionListener{
         tfphone = new JTextField();
         tfphone.setBounds(160, 310, 200, 20);
         add(tfphone);
-        
-        try{
-            
+
+        // Allow only digits for phone number input
+        tfphone.addKeyListener(new KeyAdapter() {
+            public void keyTyped(KeyEvent e) {
+                char c = e.getKeyChar();
+                if (!Character.isDigit(c) || tfphone.getText().length() >= 10) {
+                    e.consume(); // ignore non-digit or extra input
+                }
+            }
+        });
+
+        try {
             Conn c = new Conn();
-            ResultSet rs = c.s.executeQuery("select * from customer where meter_no = '"+meter+"'");
-            
-            while (rs.next()){
+            ResultSet rs = c.s.executeQuery("select * from customer where meter_no = '" + meter + "'");
+
+            while (rs.next()) {
                 name.setText(rs.getString("name"));
                 tfaddress.setText(rs.getString("address"));
                 tfcity.setText(rs.getString("city"));
@@ -95,61 +104,69 @@ public class UpdateInformation extends JFrame implements ActionListener{
                 tfphone.setText(rs.getString("phone"));
                 meternumber.setText(rs.getString("meter_no"));
             }
-            
-        }catch(Exception e){
+
+        } catch (Exception e) {
             e.printStackTrace();
         }
-        
+
         update = new JButton("Update");
         update.setBackground(Color.black);
         update.setForeground(Color.white);
         update.setBounds(80, 350, 100, 30);
         add(update);
         update.addActionListener(this);
-        
+
         cancel = new JButton("Cancel");
         cancel.setBackground(Color.black);
         cancel.setForeground(Color.white);
         cancel.setBounds(200, 350, 100, 30);
         add(cancel);
         cancel.addActionListener(this);
-        
+
         ImageIcon i1 = new ImageIcon(ClassLoader.getSystemResource("icon/update.jpg"));
         Image i2 = i1.getImage().getScaledInstance(400, 300, Image.SCALE_DEFAULT);
         ImageIcon i3 = new ImageIcon(i2);
         JLabel image = new JLabel(i3);
         image.setBounds(550, 50, 400, 300);
         add(image);
-        
-        setVisible(true); 
-        
+
+        setVisible(true);
     }
-    
-    public void actionPerformed(ActionEvent ae){
-        if(ae.getSource() == update){
+
+    public void actionPerformed(ActionEvent ae) {
+        if (ae.getSource() == update) {
             String address = tfaddress.getText();
             String city = tfcity.getText();
             String state = tfstate.getText();
             String email = tfemail.getText();
             String phone = tfphone.getText();
-            
-            try{
+
+            if (!email.endsWith("@gmail.com")) {
+                JOptionPane.showMessageDialog(null, "Please enter a valid Gmail address ending with @gmail.com");
+                return;
+            }
+
+            if (!phone.matches("\\d{10}")) {
+                JOptionPane.showMessageDialog(null, "Please enter a valid 10-digit phone number.");
+                return;
+            }
+
+            try {
                 Conn c = new Conn();
-                c.s.executeUpdate("update customer set address = '"+address+"', city = '"+city+"', state = '"+state+"', email = '"+email+"', phone = '"+phone+"' where meter_no ='"+meter+"'");
-                
+                c.s.executeUpdate("update customer set address = '" + address + "', city = '" + city + "', state = '" + state + "', email = '" + email + "', phone = '" + phone + "' where meter_no ='" + meter + "'");
+
                 JOptionPane.showMessageDialog(null, "Your details were updated successfully.");
                 setVisible(false);
-                
-            }catch(Exception e){
+
+            } catch (Exception e) {
                 e.printStackTrace();
             }
-        }else{
+        } else {
             setVisible(false);
         }
     }
-    
-    public static void main(String[] args){
+
+    public static void main(String[] args) {
         new UpdateInformation("");
     }
-    
 }
